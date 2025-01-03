2e6b16d90a1d949d43adaad1c844a75ef4d95218
Simen Strømsnes <simen@simsine.no>
2025-01-03 15:25:05 +0100

    cursed borrowing

--- aoc24_rust/day1/src/main.rs
+++ aoc24_rust/day1/src/main.rs
@@ -1,7 +1,12 @@
 
 fn main() {
-    let mut similarity_score  = 0;
+    let (left, right) = get_input();
 
+    println!("Answer to day 1 part 1: {}", part_1(&left, &right));
+    println!("Answer to day 1 part 1: {}", part_2(&left, &right));
+}   
+
+fn get_input() -> (Vec<i32>, Vec<i32>) {
     let mut left = Vec::new();
     let mut right = Vec::new();
 
@@ -18,9 +23,20 @@ fn main() {
     left.sort_unstable();
     right.sort_unstable();
 
-    while !left.is_empty() {
-        similarity_score += i32::abs(left.pop().unwrap() - right.pop().unwrap());
-    }
+    (left, right)
+}
 
-    println!("Answer to day 1 part 1: {similarity_score}")
+fn part_1(left:&Vec<i32>, right:&Vec<i32>) -> i32 {
+    left.iter().zip(right.iter())
+        .map(|(l, r)| (l-r).abs())
+        .sum()
+}
+
+fn part_2(left:&Vec<i32>,  right:&Vec<i32>) -> i32 {
+    let mut similatity_score = 0;
+    for left_num in left {
+        let in_right = right.iter().filter(|t| **t == *left_num).count() as i32;
+        similatity_score += left_num * in_right;
+    }
+    return similatity_score;
 }
